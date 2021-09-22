cf749f59804204b22959046e104e01e0e5f5e149
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2021-09-22 02:25:04 -0500

    dependency poc

---
+++ features-test/ethsigner/src/main.rs
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use marine_rs_sdk::{marine, module_manifest, WasmLoggerBuilder};
+
+module_manifest!();
+
+pub fn main() {
+    WasmLoggerBuilder::new().build().unwrap();
+}
+
+#[marine]
+pub fn greeting(name: String) -> String {
+    format!("Hi, {}", name)
+}
+
+// To run tests:
+// cargo test --release
+// Since the unit tests are using the wasm module via the marine_test crate import
+// the modules and Config.toml need to exist.
+// That is, run ./build.sh before you run cargo test.
+// Moreover, the test function(s) need to be prefixed by the wasm module namespace, which
+// generally is derived from the project name.
+// For example, if you name the project "greeting", e.g., cargo generate -g https:// ... --name greeting,
+// the test below can be executed as is. If not, the project needs to replace the "greeting"
+// reference in place. Let's say our project name is "greetproject" then you need to update the type to:
+// fn test_greeting(greeting: marine_test_env::greetproject::ModuleInterface)
+#[cfg(test)]
+mod tests {
+    use marine_rs_sdk_test::marine_test;
+
+    #[marine_test(config_path = "../Config.toml", modules_dir = "../artifacts")]
+    fn test_greeting(greeting: marine_test_env::greeting::ModuleInterface) {
+        let name = "Marine";
+        let res = greeting.greeting(name.to_string());
+        assert_eq!(res, format!("Hi, {}", name));
+    }
+}
